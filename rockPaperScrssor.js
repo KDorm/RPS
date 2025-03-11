4deaef0625bf2d06e5ebbf16c8eaeb7b2179c2e9
Kyle Dorman <dormank87@gmail.com>
2025-03-11 13:37:13 -0400

    added comments to js file and changed background on css

--- rockPaperScrssor.js
+++ rockPaperScrssor.js
@@ -1,4 +1,4 @@
-function getComputerChoice(){
+function getComputerChoice(){ //random computer choice, returns string
     cpu = Math.floor((Math.random() * 3) + 1); //Math.random returns a number 0-1 * 3 (0-2) + 1 gives 1-3.  Then round down with Math.floor.
     if(cpu == 1){
         return "Rock";
@@ -10,8 +10,8 @@ function getComputerChoice(){
         return "Scissros";
     }   
 }
-function getUserChoice(){
-    let choice = prompt("Enter Rock Paper or Scissors:");
+function getUserChoice(){//takes user input and returns string
+    let choice = prompt("Enter Rock Paper or Scissors:"); // takes in string from user and immediatly puts in lower case so user can type in ROCK ROck or rock
     choice = choice.toLowerCase();
 
     if(choice == "rock"){
@@ -24,7 +24,7 @@ function getUserChoice(){
         return "Scissors";
     } 
 }
-function playRound(userChoice, cpuChoice){
+function playRound(userChoice, cpuChoice){//plays one round and keeps score, values from getComputerChoice and getUserChoice are passed in
     
     if(userChoice == "Rock"){
         console.log("You played Rock");
@@ -79,14 +79,14 @@ function playRound(userChoice, cpuChoice){
 }
 
 
-let userCount = 0;
+let userCount = 0;//counter values are kept outside the function or the count would reset to 0 after every round
 let cpuCount = 0;
-let roundCount = 1;
+let roundCount = 1;//sentinal value to stop game at 5 rounds
 
-while(roundCount <= 5){
-    let userMove = getUserChoice(); 
-    let cpuMove = getComputerChoice();
-    playRound(userMove, cpuMove);
+while(roundCount <= 5){//loop allows for 5 rounds
+    let userMove = getUserChoice(); //users choice is stored in userMove
+    let cpuMove = getComputerChoice();//cpu random choice is stored in cpuMove
+    playRound(userMove, cpuMove);//both user and cpu move are passed in play round to play one round
 
     roundCount++;
 }
